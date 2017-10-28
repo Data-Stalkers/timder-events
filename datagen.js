2ff08af7c7739f513185c746b51b35cf0b8f876a
Marcus Polk <marcuspolk96@gmail.com>
2017-10-28 15:44:40 -0700

    writes direclty to mongo

--- datagen.js
+++ datagen.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
-// const mongo = require('')
-// format for timestamp: 1999-01-08 04:05:06
+const MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
+// format for timestamp should be: 1999-01-08 04:05:06
 const genMatch = function() {
   let auserid = Math.floor(Math.random() * 10000000);
   let buserid = Math.floor(Math.random() * 10000000);
@@ -9,29 +10,33 @@ const genMatch = function() {
   return {auserid, buserid, ts}
 }
 
-const generate = function(num) {
-  if (num > 1000000) {
-    while (num > 1000000) {
-      generate(1000000);
-      num -= 1000000;
-    }
-    generate(num);
-    return;
-  }
-  let matchStream = fs.createWriteStream('matchData.json', {'flags': 'a'});
-  let swipeStream = fs.createWriteStream('swipeData.json', {'flags': 'a'});
+const insertMatch = function(db, match, callback) {
+  // Get the documents colllection
+  let matches = db.collection('matches');
+  let swipes = db.collection('swipes');
+  let swipeA = {auserid: match.auserid, buserid: match.buserid, swipe: 1, ts: match.ts};
+  let swipeB = {auserid: match.buserid, buserid: match.auserid, swipe: 1, ts: match.ts};
+  // Insert some documents.
+  matches.insert(match, function(err) {
+    if (err) console.log(err)
+  });
+  swipes.insertMany([swipeA, swipeB], function(err) {
+    if (err) console.log(err)
+  });
+}
 
-  for (let i = 1; i <= num; i++) {
-    let match = genMatch();
-    matchStream.write(JSON.stringify(match) + '\n');
-    let swipeA = {auserid: match.auserid, buserid: match.buserid, swipe: 1, ts: match.ts};
-    let swipeB = {auserid: match.buserid, buserid: match.auserid, swipe: 1, ts: match.ts};
-    swipeStream.write(JSON.stringify(swipeA) + '\n');
-    swipeStream.write(JSON.stringify(swipeB) + '\n');
-    if (i % 250000 === 0) console.log(i);
-  }
-  matchStream.end();
-  swipeStream.end();
+
+const generate = function(num) {
+  const matchurl = 'mongodb://localhost:27017/tinderevents';
+  // Use connect method to connect to the Server
+  MongoClient.connect(matchurl, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    for (let i = 1; i <= num; i++) {
+      insertMatch(db, genMatch(), (result)=> console.log(i))
+      if (i % 250000 === 0) console.log(i);
+    }
+  });
 }
 
 generate(process.argv[2]);
