d3d7ff5da4d3d3050bc75255769ec400460d2897
Marcus Polk <marcuspolk96@gmail.com>
2017-10-31 09:55:28 -0700

    datagen optimized. back to one stream that waits for drain events

--- datagen.js
+++ datagen.js
@@ -1,49 +1,39 @@
+console.time('execution'); // program generates 10m entries in 21566ms.
 const fs = require('fs');
 const MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
-// format for timestamp should be: 1999-01-08 04:05:06
-const genTimestamp = function() {
+const timestamp = function() {
   let year = '2017';
   let month = ['08', '09', '10'][Math.floor(Math.random() * 3)];
   let day = Math.floor(Math.random() * 30) + 1;
   if (day < 10) day = '0' + day;
   return `${year}-${month}-${day}`;
 }
-const genMatch = function() {
-  let auserid = Math.floor(Math.random() * 10000000);
-  let buserid = Math.floor(Math.random() * 10000000);
-  let ts = genTimeStamp();
-  return {auserid, buserid, ts}
+
+const swipe = function() {
+  let swipe = Math.random() < 0.56 ? 1 : 0;
+  let match = 0;
+  if (swipe === 1) match = Math.random() < 0.34 ? 1 : 0;
+  return `${Math.floor(Math.random() * 10000000)},${Math.floor(Math.random() * 10000000)},${swipe},${match},${timestamp()}\n`
 }
 
-const insertMatch = function(db, match, callback) {
-  // Get the documents colllection
-  let matches = db.collection('matches');
-  let swipes = db.collection('swipes');
-  let swipeA = {auserid: match.auserid, buserid: match.buserid, swipe: 1, ts: match.ts};
-  let swipeB = {auserid: match.buserid, buserid: match.auserid, swipe: 1, ts: match.ts};
-  // Insert some documents.
-  matches.insert(match, function(err) {
-    if (err) console.log(err)
-  });
-  swipes.insertMany([swipeA, swipeB], function(err) {
-    if (err) console.log(err)
-  });
-}
+const swipeStream = fs.createWriteStream('../swipes.csv');
+let count = process.argv[2];
+let ok = true;
 
-
-const generate = function(num) {
-  const matchurl = 'mongodb://localhost:27017/tinderevents';
-  // Use connect method to connect to the Server
-  MongoClient.connect(matchurl, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
-    for (let i = 1; i <= num; i++) {
-      insertMatch(db, genMatch(), (result)=> console.log(i))
-      if (i % 250000 === 0) console.log(i);
+let write = function(callback) {
+  do {
+    count--;
+    if (count % 100000 === 0) console.log(count);
+    if (count === 0) { //last write.
+      swipeStream.write(swipe(), 'utf8');
+      console.timeEnd('execution');
     }
-  });
+    ok = swipeStream.write(swipe(), 'utf8');
+  } while (count > 0 && ok)
+  if (count > 0) {
+    swipeStream.once('drain', write);
+  }
 }
-
-generate(process.argv[2]);
+write();
