8a043d9ca0a789557065f8cf03ca04f33c7d3f22
Marcus Polk <marcuspolk96@gmail.com>
2017-11-01 12:26:52 -0700

    elastic init can pull swipe counts for each day.

--- elasticInit.js
+++ elasticInit.js
@@ -2,13 +2,37 @@
 // feed that into elasticsearch.
 
 // NOTE: It's a good idea to create an index for ts and match before running this file.
-
-let months = ['08', '09', '10'];
+const mongo = require('mongodb').MongoClient
+const elastic = require('elasticsearch');
+const url = 'mongodb://localhost:27017/tinder'
+const months = ['08', '09', '10'];
 let days = []
 for (let i = 1; i <= 30; i++) {
   if (i < 10) days.push('0'+ i);
   else days.push('' + i);
 }
+
+mongo.connect(url, (err, db) => {
+  if (err) console.log('ERROR CONNECTING TO MONGO.', err);
+  else {
+    console.log('Successfully connected to MongoDB');
+    const swipes = db.collection('swipes');
+    let completed = 0;
+    for (month of months) {
+      for (day of days) {
+        let date = `2017-${month}-${day}`;
+        swipes.find({'ts': date}).count((err, count) => {
+          console.log(`ts: ${date}, count: ${count}`)
+          if (++completed === 90) db.close()
+        })
+      }
+    }
+  }
+})
+
+
+
+
 
 
 
