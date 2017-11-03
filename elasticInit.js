b7c0fa2e53c2ffa26c4cd4c715b48a4e2e80bb94
Marcus Polk <marcuspolk96@gmail.com>
2017-11-03 09:48:51 -0700

    server setup to push to elastic

--- elasticInit.js
+++ elasticInit.js
@@ -3,16 +3,21 @@
 
 // NOTE: It's a good idea to create an index for ts and match before running this file.
 const mongo = require('mongodb').MongoClient
-const elastic = require('elasticsearch');
-const url = 'mongodb://localhost:27017/tinder'
+var elasticsearch = require('elasticsearch');
+const elasticClient = new elasticsearch.Client({
+  host: '127.0.0.1:9200'
+});
+
+const mongourl = 'mongodb://localhost:27017/tinder'
 const months = ['08', '09', '10'];
 let days = []
 for (let i = 1; i <= 30; i++) {
   if (i < 10) days.push('0'+ i);
   else days.push('' + i);
 }
+let dateCount = {}
 
-mongo.connect(url, (err, db) => {
+mongo.connect(mongourl, (err, db) => {
   if (err) console.log('ERROR CONNECTING TO MONGO.', err);
   else {
     console.log('Successfully connected to MongoDB');
@@ -22,18 +27,20 @@ mongo.connect(url, (err, db) => {
       for (day of days) {
         let date = `2017-${month}-${day}`;
         swipes.find({'ts': date}).count((err, count) => {
-          console.log(`ts: ${date}, count: ${count}`)
-          if (++completed === 90) db.close()
-        })
+          dateCount[date] = count;
+          if (++completed === 90) {
+            db.close();
+            elasticPost(dateCount);}
+        });
       }
     }
   }
-})
+});
 
+let elasticPost = function(dateCount) {
+  // query for each item in dateCount. do a write.
 
-
-
-
+}
 
 
  /**
